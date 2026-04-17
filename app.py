dca6236a02d1527507aa777357e5c80ef9e79d4a
nesrine-dev <nesrinebendhia65@gmail.com>
2026-04-18 01:30:47 +0300

    app.py

--- app.py
+++ app.py
@@ -393,4 +393,7 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=os.environ.get("FLASK_DEBUG", "false") == "true")
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000))
+    )
